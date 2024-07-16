08c3a44ce3a37331d4153a30b7c7c31ca8c8f15f
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-07-16 19:07:21 +0530

    Create ChatTab.dart

---
+++ lib/tabs/ChatTab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatTab extends StatefulWidget {
+  const ChatTab({super.key});
+
+  @override
+  State<ChatTab> createState() => _ChatTabState();
+}
+
+class _ChatTabState extends State<ChatTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
