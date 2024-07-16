7fdf28d9345b9734f9ac205c22713a8bd116fab9
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-07-16 18:38:35 +0530

    Create styles.dart

---
+++ lib/styles/styles.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+import 'package:med_channel/styles/colors.dart';
+
+TextStyle kTitleStyle = TextStyle(
+  color: Color(MyColors.header01),
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle kFilterStyle = TextStyle(
+  color: Color(MyColors.bg02),
+  fontWeight: FontWeight.w500,
+);
