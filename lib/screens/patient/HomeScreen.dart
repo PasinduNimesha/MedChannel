57bf344283ed37bce9e6bc956dab845d8995963d
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-07-16 18:45:00 +0530

    Update HomeScreen.dart

--- lib/screens/patient/HomeScreen.dart
+++ lib/screens/patient/HomeScreen.dart
@@ -1,57 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:med_channel/styles/colors.dart';
+import 'package:med_channel/tabs/HomeTab.dart';
+import 'package:med_channel/tabs/ScheduleTab.dart';
 
-class Homescreen extends StatefulWidget {
-  const Homescreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<Homescreen> createState() => _HomescreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomescreenState extends State<Homescreen> {
+List<Map> navigationBarItems = [
+  {'icon': Icons.local_hospital, 'index': 0},
+  {'icon': Icons.calendar_today, 'index': 1},
+];
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  void goToSchedule() {
+    setState(() {
+      _selectedIndex = 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> screens = [
+      HomeTab(
+        onPressedScheduleCard: goToSchedule,
+      ),
+      ScheduleTab(),
+    ];
+
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          height: MediaQuery.of(context).size.height - 50,
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  const SizedBox(height: 60.0),
-                  const Text(
-                    "Welcome",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
+      appBar: AppBar(
+        backgroundColor: Color(MyColors.primary),
+        elevation: 0,
+        toolbarHeight: 0,
+      ),
+      body: SafeArea(
+        child: screens[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedFontSize: 0,
+        selectedItemColor: Color(MyColors.primary),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: [
+          for (var navigationBarItem in navigationBarItems)
+            BottomNavigationBarItem(
+              icon: Container(
+                height: 55,
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: _selectedIndex == navigationBarItem['index']
+                        ? BorderSide(color: Color(MyColors.bg01), width: 5)
+                        : BorderSide.none,
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "You are logged in",
-                    style: TextStyle(fontSize: 15, color: Colors.grey[700]),
-                  )
-                ],
+                ),
+                child: Icon(
+                  navigationBarItem['icon'],
+                  color: _selectedIndex == 0
+                      ? Color(MyColors.bg01)
+                      : Color(MyColors.bg02),
+                ),
               ),
-              Column(
-                children: <Widget>[
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/login');
-                    },
-                    child: const Text('Logout'),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
+              label: '',
+            ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (value) => setState(() {
+          _selectedIndex = value;
+        }),
       ),
     );
   }
