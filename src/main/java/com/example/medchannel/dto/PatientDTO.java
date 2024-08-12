3f20927eb57500456ea23243fa5f20057a90f457
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-08-12 20:15:37 +0530

    Update PatientDTO.java

--- src/main/java/com/example/medchannel/dto/PatientDTO.java
+++ src/main/java/com/example/medchannel/dto/PatientDTO.java
@@ -2,6 +2,8 @@ package com.example.medchannel.dto;
 
 public record PatientDTO(String patient_id,
                          String id,
+                         String first_name,
+                         String last_name,
                          String address,
                          String phone,
                          String gender,
