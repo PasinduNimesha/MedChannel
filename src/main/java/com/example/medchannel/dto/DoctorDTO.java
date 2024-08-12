b55b9ac26b402578c78c6be5c46650f0e409b883
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-08-12 20:15:36 +0530

    Update DoctorDTO.java

--- src/main/java/com/example/medchannel/dto/DoctorDTO.java
+++ src/main/java/com/example/medchannel/dto/DoctorDTO.java
@@ -2,6 +2,8 @@ package com.example.medchannel.dto;
 
 public record DoctorDTO(String doc_id,
                      String id,
+                     String first_name,
+                     String last_name,
                      boolean available,
                      int experience,
                      String specialty,
