28ee2aa2779eba2d6ebcc0c3c43f0baa89cc2104
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-08-13 21:10:14 +0530

    Update RegistrationScreen.dart

--- lib/screens/patient/RegistrationScreen.dart
+++ lib/screens/patient/RegistrationScreen.dart
@@ -11,17 +11,7 @@ class _RegistrationscreenState extends State<Registrationscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Registration'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('Registration Screen'),
-          ],
-        ),
-      ),
+
     );
   }
 }
