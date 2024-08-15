59134447c23aa0021c004e02deabc03e8f2a28dd
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-08-15 20:37:24 +0530

    Create PhysicianHomeTab.dart

---
+++ lib/tabs/physician/PhysicianHomeTab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Physicianhometab extends StatefulWidget {
+  const Physicianhometab({super.key});
+
+  @override
+  State<Physicianhometab> createState() => _PhysicianhometabState();
+}
+
+class _PhysicianhometabState extends State<Physicianhometab> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
