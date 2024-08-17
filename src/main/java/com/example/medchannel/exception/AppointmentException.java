36223e2c38d2dac8b4ad4a8b9fe087f9e3fa0c0d
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-08-17 10:06:13 +0530

    Create AppointmentException.java

---
+++ src/main/java/com/example/medchannel/exception/AppointmentException.java
@@ -0,0 +1,7 @@
+package com.example.medchannel.exception;
+
+public class AppointmentException extends RuntimeException {
+    public AppointmentException(String message) {
+        super(message);
+    }
+}
