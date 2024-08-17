35f91cfabd0d02f16a23fff0b2474daeca441a8b
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-08-17 14:24:21 +0530

    Update AppointmentDTO.java

--- src/main/java/com/example/medchannel/dto/AppointmentDTO.java
+++ src/main/java/com/example/medchannel/dto/AppointmentDTO.java
@@ -5,6 +5,7 @@ public record AppointmentDTO(String appointment_id,
                              String doc_id,
                              String appointment_date,
                              String appointment_time,
+                             String remarks,
                              String created_at,
                              String updated_at) {
 }
