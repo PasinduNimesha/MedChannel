eff1ae8a4a138332f4e2f449fc7f59d7d6aafd71
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-08-17 10:06:23 +0530

    Create PhysicianDTO.java

---
+++ src/main/java/com/example/medchannel/dto/PhysicianDTO.java
@@ -0,0 +1,13 @@
+package com.example.medchannel.dto;
+
+public record PhysicianDTO(String doc_id,
+                     String first_name,
+                     String last_name,
+                     boolean available,
+                     int experience,
+                     String specialty,
+                     String hospital,
+                     String license_no,
+                     String created_at,
+                     String updated_at) {
+}
