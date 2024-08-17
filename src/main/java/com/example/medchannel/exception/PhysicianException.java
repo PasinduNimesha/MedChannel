5ccaca8ddd2177b4987241a707de588c61ef753b
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-08-17 10:05:46 +0530

    Create PhysicianException.java

---
+++ src/main/java/com/example/medchannel/exception/PhysicianException.java
@@ -0,0 +1,7 @@
+package com.example.medchannel.exception;
+
+public class PhysicianException extends RuntimeException {
+    public PhysicianException(String message) {
+        super(message);
+    }
+}
