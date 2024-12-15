8aa2a37823a0a5198f06a1abe8e3ec811de7b8c7
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-12-15 13:22:37 +0530

    Update PatientServiceImpl.java

--- src/main/java/com/example/medchannel/service/PatientServiceImpl.java
+++ src/main/java/com/example/medchannel/service/PatientServiceImpl.java
@@ -5,19 +5,25 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.example.medchannel.dto.PatientDTO;
 import com.example.medchannel.dto.ResponseDTO;
 import com.example.medchannel.entity.Patient;
 import com.example.medchannel.exception.PatientException;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -152,5 +158,24 @@ public class PatientServiceImpl implements PatientService {
         expectedAttributeValueMap.put("id", new ExpectedAttributeValue(new AttributeValue().withS(Patient.getPatient_id())));
         dynamoDBSaveExpression.setExpected(expectedAttributeValueMap);
         return dynamoDBSaveExpression;
+    }
+
+    private final AmazonS3 amazonS3;
+
+
+    public String uploadProfilePicture(MultipartFile file) throws IOException {
+        String fileName = "profile-pictures/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+
+        // Add metadata for the file
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        metadata.setContentType(file.getContentType());
+
+        // Upload to S3
+        String bucketName = "medchannel.profilepics";
+        amazonS3.putObject(bucketName, fileName, file.getInputStream(), metadata);
+
+        // Return the file URL
+        return amazonS3.getUrl(bucketName, fileName).toString();
     }
 }
