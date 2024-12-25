a165b57e242b393dfad952c97bf18da5443d50bf
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-12-25 18:50:08 +0530

    Update PhysicianHomeTab.dart

--- lib/tabs/physician/PhysicianHomeTab.dart
+++ lib/tabs/physician/PhysicianHomeTab.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../styles/colors.dart';
+import '../../styles/styles.dart';
+import '../patient/PatientHomeTab.dart';
+
 class Physicianhometab extends StatefulWidget {
   const Physicianhometab({super.key});
 
@@ -10,6 +14,66 @@ class Physicianhometab extends StatefulWidget {
 class _PhysicianhometabState extends State<Physicianhometab> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            UserIntro(patientId: ""),
+            SizedBox(
+              height: 10,
+            ),
+            SearchInput(),
+            SizedBox(
+              height: 20,
+            ),
+            CategoryIcons(),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Appointment Today',
+                  style: kTitleStyle,
+                ),
+                TextButton(
+                  child: Text(
+                    'See All',
+                    style: TextStyle(
+                      color: Color(MyColors.yellow01),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Top Doctor',
+              style: TextStyle(
+                color: Color(MyColors.header01),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
