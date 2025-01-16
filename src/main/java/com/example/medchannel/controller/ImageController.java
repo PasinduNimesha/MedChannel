b1477c01db567f7857363cf0ac75c44a96403739
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2025-01-16 12:28:55 +0530

    Update ImageController.java

--- src/main/java/com/example/medchannel/controller/ImageController.java
+++ src/main/java/com/example/medchannel/controller/ImageController.java
@@ -35,7 +35,6 @@ public class ImageController {
             // Get the original filename
             String originalFilename = Objects.requireNonNull(file.getOriginalFilename());
 
-            // Validate the file type (only allow image files)
             if (!file.getContentType().startsWith("image/")) {
                 System.out.println("Invalid file type: " + file.getContentType());
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid file type. Only images are allowed!");
